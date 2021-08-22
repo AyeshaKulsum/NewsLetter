8c95ba4a78e6d8821d6c79c141daf75737b3f384
Ayesha Kulsum S J <36045201+AyeshaKulsum@users.noreply.github.com>
2021-08-22 17:33:40 +0530

    clean code

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-//DB Connection
-module.exports = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
-  host: process.env.HOST,
-  dialect: process.env.DIALECT,
-  logging: false
-});
--- backend/config/redis.js
+++
@@ -1,8 +0,0 @@
-const redis = require("redis");
-const client = redis.createClient(console.log('Redis is connected'));
-
-client.on("error", function (error) {
-    console.error(error);
-});
-
-module.exports = client
